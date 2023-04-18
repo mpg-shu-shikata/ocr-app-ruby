11adfec2e33921868b9d1f61c8e06c6109fcd9cb
mpg-shu-shikata <shu.shikata@medpeer.co.jp>
2023-04-19 00:09:54 +0900

    gcf-base

---
+++ app.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'functions_framework'
+require 'google/cloud/logging'
+
+FunctionsFramework.http 'image' do |request|
+end
+
+FunctionsFramework.http 'pdf' do |request|
+end
