98f69afa7309933524ceafab0fbc5a6eca19a15b
mpg-shu-shikata <shu.shikata@medpeer.co.jp>
2023-04-21 12:50:36 +0900

    fix

--- app.rb
+++ app.rb
@@ -7,39 +7,23 @@ require './app/image_annotator'
 require './app/text_completer'
 
 FunctionsFramework.http 'image' do |request|
-  headers = {
-    'Access-Control-Allow-Origin' => '*',
-    'Access-Control-Allow-Methods' => '*',
-    'Access-Control-Allow-Headers' => '*',
-    'Access-Control-Max-Age' => '3600'
-  }
-
   params = JSON.parse(request.body.read.to_s, symbolize_names: true)
 
   logger.info '========================================'
   logger.info params
   logger.info '========================================'
+
   content = ImageAnnotator.call(params[:binaryData])
-
-  logger.info content
-
-  result = TextCompleter.call(content)
-
-  logger.info result
-
-  [200, headers, [result]]
+  TextCompleter.call(content)
 end
 
 FunctionsFramework.http 'pdf' do |request|
-  headers = {
-    'Access-Control-Allow-Origin' => '*',
-    'Access-Control-Allow-Methods' => '*',
-    'Access-Control-Allow-Headers' => '*',
-    'Access-Control-Max-Age' => '3600'
-  }
   params = JSON.parse(request.body.read.to_s, symbolize_names: true)
-  content = PdfAnnotator.call(params[:binaryData])
-  result = TextCompleter.call(content)
 
-  [200, headers, [result]]
+  logger.info '========================================'
+  logger.info params
+  logger.info '========================================'
+
+  content = PdfAnnotator.call(params[:binaryData])
+  TextCompleter.call(content)
 end
