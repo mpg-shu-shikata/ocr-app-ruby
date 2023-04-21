370b9aed96a1b918a7883a27e365080dad5a46d8
yuya.tani <yuya.tani@medpeer.co.jp>
2023-04-21 09:48:38 +0900

    メソッドの切り方などリファクタリング

--- app/pdf_annotator.rb
+++ app/pdf_annotator.rb
@@ -8,40 +8,48 @@ require "google/cloud/storage"
 Dotenv.load
 
 class PdfAnnotator
+  CREDENTIAL_FILE_PATH = './credentials.json'.freeze
+  BUCKET_NAME = 'ocr-app-bucket-for-pdf'.freeze
+
   def self.call(base64_str)
     new(base64_str).call
   end
 
   def initialize(base64_str)
-    @base64_str = base64_str
+    # TODO: フロントとの繋ぎ込みが完了したら切り替える
+    # @base64_str = base64_str
+    @base64_str = Base64.encode64(File.read('test.pdf'))
+    @file_name = "#{SecureRandom.uuid}.pdf"
+    @path = File.join('./', file_name)
   end
 
   def call
-    file_name = "#{SecureRandom.uuid}.pdf"
-    path = File.join('./', file_name)
-
-    # TODO: フロントとの繋ぎ込みが完了したら削除する
-    # @base64_str = Base64.encode64(File.read('test.pdf'))
-    # File.open(path, 'wb') do |f|
-    #   f.puts Base64.decode64(base64_str)
-    #   f.flush
-    # end
-
-    bucket = storage.bucket "ocr-app-bucket-for-pdf"
-    bucket.create_file(path, file_name)
-
-    response = client.batch_annotate_files(requests: build_params("gs://ocr-app-bucket-for-pdf/#{file_name}"))
+    google_storage_file = upload_pdf_to_google_storage
+    response = annotate_file
+    google_storage_file.delete
     extract_pdf_text(response.to_h)
   end
 
   private
 
-  attr_reader :base64_str
+  attr_reader :base64_str, :file_name, :path
 
-  def storage
-    Google::Cloud::Storage.new(
-      credentials: "./credentials.json"
+  def upload_pdf_to_google_storage
+    File.open(path, 'wb') do |f|
+      f.puts Base64.decode64(base64_str)
+      f.flush
+    end
+
+    file = google_storage_bucket.create_file(path, file_name)
+    File.delete(path)
+    file
+  end
+
+  def google_storage_bucket
+    storage = Google::Cloud::Storage.new(
+      credentials: CREDENTIAL_FILE_PATH
     )
+    storage.bucket BUCKET_NAME
   end
 
   def client
@@ -66,6 +74,10 @@ class PdfAnnotator
         ]
       }
     ]
+  end
+
+  def annotate_file
+    client.batch_annotate_files(requests: build_params("gs://#{BUCKET_NAME}/#{file_name}"))
   end
 
   def extract_pdf_text(google_vision_response)
